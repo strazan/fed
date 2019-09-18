1b6b9f0318ddde7b5cd85557b0d548ce38ee4267
Sigge Labor <sigge@labor.cool>
2019-09-18 16:22:23 +0200

    fun with apis - sigges revenue  charts

--- javascript-intro/fun-with-apis/sigges-charts /script.js
+++ javascript-intro/fun-with-apis/sigges-charts /script.js
@@ -1,46 +1,79 @@
 var revenue = [];
 var years = [];
 var chart;
+var company;
 var ctx = document.getElementById('myChart').getContext('2d');
+loadChart('Apple');
 
-function loadValue() {
 
-    fetch('https://financialmodelingprep.com/api/v3/financials/income-statement/aapl')
+function fetchMoney(companyShort) {
+
+    fetch('https://financialmodelingprep.com/api/v3/financials/income-statement/' + companyShort + '?period=quarter')
         .then(function (response) {
             return response.json();
         })
         .then(function (myJson) {
-
-            var i;
+            revenue = [];
+            years = [];
             for (i = (myJson.financials.length - 1); i > 0; i--) {
                 revenue.push(myJson.financials[i].Revenue);
-                years.push(myJson.financials[i].date.substring(0, 4));
+                years.push(myJson.financials[i].date.substring(0, 7));
             }
-            chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: years,
-                    datasets: [{
-                        label: 'Apple revenue',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: revenue
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                // Abbreviate the millions
-                                callback: function (value, index, values) {
-                                    return value / 1e9 + 'B';
-                                }
-                            }
-                        }]
-                    }
-                }
-            });
+            updateChart(company)
         });
 }
 
-loadValue();
+function loadChart(comp) {
+    company = comp;
+    switch (company) {
+        case 'Apple':
+            fetchMoney('aapl');
+            break;
+
+        case 'Alphabet':
+            fetchMoney('goog');
+            break;
+
+        case 'Facebook':
+            fetchMoney('fb');
+            break;
+
+        case 'Tesla':
+            fetchMoney('tsla');
+            break;
+
+        case 'Amazon':
+            fetchMoney('amzn');
+            break;
+    }
+
+}
+
+function updateChart(company) {
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: years,
+            datasets: [{
+                label: company + ' revenue',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: revenue
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // Abbreviate the millions
+                        callback: function (value, index, values) {
+                            return value / 1e9 + 'B';
+                        }
+                    }
+                }]
+            }
+        }
+    });
+    
+}
